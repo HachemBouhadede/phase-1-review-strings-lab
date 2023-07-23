c8a4b8d74e1ddd9a3568cf27ff59771de39a28cf
Hachem <hachemdz25@gmail.com>
2023-07-23 18:21:46 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// assigne name
+const currentUser = 'Hachoum';
+//welcome message 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+// excited welcoming message
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// quick greeeting
+const shortGreeting = `Welcome, `+ `${currentUser.slice(0, 1)}`+`!`; 
